ca2877408306718ed049b94c2202d3daa488f1c2
lorenzo <laurentiu.oprea09@gmail.com>
2025-05-07 21:06:09 +0300

    Adding first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10,20,30,40,50};
+
+        for (int number : numbers){
+            System.out.println("Test" + number);
+
+        }
+
+
+
+
+        }
+    }
